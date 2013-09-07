514ce71a6f9ac5a5b114b64f9ffb6ae6ffe992cc
ub12 <ub12@ub12-VirtualBox.(none)>
2013-09-07 13:26:15 -0500

    alternative title

--- content_scripts.js
+++ content_scripts.js
@@ -8,7 +8,7 @@ var saveLinkText = function(e) {
   var currURL = document.URL;
   var linkText = e.target.innerHTML;
   var linkURL = e.target.href;
-  chrome.runtime.sendMessage({linkfollowed: "yes", parent_URL: currURL, link_Text: linkText, link_URL: linkURL}, function(response){ });
+  chrome.runtime.sendMessage({type: "link", linkfollowed: "yes", parent_URL: currURL, link_Text: linkText, link_URL: linkURL}, function(response){ });
 };
 
 // Set up link handlers
@@ -16,4 +16,4 @@ for (var i = 0; i < length; i++) {
   links[i].addEventListener('click', saveLinkText , false); 
 }
 
-
+chrome.runtime.sendMessage({type: "load", page_url: document.URL, page_title: document.getElementsByTagName("title")[0].innerHTML});
